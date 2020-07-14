0dcb334ab6d29cc414c1a2dbc81da6c1dfbb3aff
Waka867 <ledwing.hernandez@gmail.com>
2020-07-14 13:08:57 -0400

    More walkthrough for const, let and template literals

--- webpack.config.js
+++ webpack.config.js
@@ -6,15 +6,15 @@ module.exports = {
   entry: './assets/js/scripts.js',
   output: {filename: './public/js/bundle.js'},
   module:{
-      loaders: [
-          {
-              test: /\.js$/,
-              exclude: /node_modules/,
-              loader: 'babel-loader',
-              query: {
-                  presets:['es2015']
-              }
-          }
-      ]
+      // loaders: [
+      //     {
+      //         test: /\.js$/,
+      //         exclude: /node_modules/,
+      //         loader: 'babel-loader',
+      //         query: {
+      //             presets:['es2015']
+      //         }
+      //     }
+      // ]
   }
 };
