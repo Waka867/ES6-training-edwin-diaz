12b09c7376f7c612b856b082bac6bf846b51cf13
Waka867 <ledwing.hernandez@gmail.com>
2020-07-14 11:04:27 -0400

    More screwing around with packages. I am following a udemy course so Im not messing too much with this code, rather being walked through examples

--- webpack.config.js
+++ webpack.config.js
@@ -4,42 +4,17 @@
 module.exports = {
 
   entry: './assets/js/scripts.js',
-  output: {filename: './public/js/bundle.js'
-
-
-  },
-
-
+  output: {filename: './public/js/bundle.js'},
   module:{
-
       loaders: [
-
-
           {
-
               test: /\.js$/,
               exclude: /node_modules/,
               loader: 'babel-loader',
               query: {
-
                   presets:['es2015']
-
               }
-
-
-
-
           }
-
-
-
       ]
-
-
   }
-
-
-
-
-
 };
